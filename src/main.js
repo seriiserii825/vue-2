2e0b223d3f2e2b5edcac4e47506d4683ec1ea14b
serii <seriiburduja@gmail.com>
2020-10-22 20:52:51 +0300

    clear all data

--- src/main.js
+++ src/main.js
@@ -1,12 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import BaseCard from './components/UI/BaseCard.vue'
-import BaseButton from './components/UI/BaseButton.vue'
 
 Vue.config.productionTip = false
-
-Vue.component('base-card', BaseCard)
-Vue.component('base-button', BaseButton)
 
 new Vue({
   render: h => h(App)
