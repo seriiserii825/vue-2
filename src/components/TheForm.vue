b10aa13ab8927c235c400baf37b99889d96fc1d5
serii <seriiburduja@gmail.com>
2020-10-22 18:25:48 +0300

    validation

--- src/components/TheForm.vue
+++ src/components/TheForm.vue
@@ -1,16 +1,17 @@
 <template>
-  <form>
-    <div class="form-control">
+  <form @submit.prevent="submitForm">
+    <div class="form-control" :class="{invalid: userNameValid === 'invalid'}">
       <label for="user-name">Your Name</label>
-      <input id="user-name" name="user-name" type="text"/>
+      <input id="user-name" name="user-name" type="text" v-model.trim="userName" @blur="validateUserName"/>
+      <p v-if="userNameValid === 'invalid'">Please enter your name</p>
     </div>
     <div class="form-control">
       <label for="age">Your Age (Years)</label>
-      <input id="age" name="age" type="number"/>
+      <input id="age" name="age" type="number" v-model.number="userAge"/>
     </div>
     <div class="form-control">
       <label for="referrer">How did you hear about us?</label>
-      <select id="referrer" name="referrer">
+      <select id="referrer" name="referrer" v-model="referrer">
         <option value="google">Google</option>
         <option value="wom">Word of mouth</option>
         <option value="newspaper">Newspaper</option>
@@ -19,30 +20,30 @@
     <div class="form-control">
       <h2>What are you interested in?</h2>
       <div>
-        <input id="interest-news" name="interest" type="checkbox"/>
+        <input id="interest-news" name="interest" type="checkbox" value="news" v-model="interest"/>
         <label for="interest-news">News</label>
       </div>
       <div>
-        <input id="interest-tutorials" name="interest" type="checkbox"/>
+        <input id="interest-tutorials" name="interest" type="checkbox" value="tutorials" v-model="interest"/>
         <label for="interest-tutorials">Tutorials</label>
       </div>
       <div>
-        <input id="interest-nothing" name="interest" type="checkbox"/>
+        <input id="interest-nothing" name="interest" value="nothing" type="checkbox" v-model="interest"/>
         <label for="interest-nothing">Nothing</label>
       </div>
     </div>
     <div class="form-control">
       <h2>How do you learn?</h2>
       <div>
-        <input id="how-video" name="how" type="radio"/>
+        <input id="how-video" name="how" type="radio" value="video-courses" v-model="how"/>
         <label for="how-video">Video Courses</label>
       </div>
       <div>
-        <input id="how-blogs" name="how" type="radio"/>
+        <input id="how-blogs" name="how" type="radio" value="blogs" v-model="how"/>
         <label for="how-blogs">Blogs</label>
       </div>
       <div>
-        <input id="how-other" name="how" type="radio"/>
+        <input id="how-other" name="how" type="radio" value="other" v-model="how"/>
         <label for="how-other">Other</label>
       </div>
     </div>
@@ -51,8 +52,38 @@
     </div>
   </form>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      userName: '',
+      userAge: null,
+      referrer: 'wom',
+      interest: ['news'],
+      how: 'blogs',
+      userNameValid: 'pending'
+    }
+  },
+  methods: {
+    submitForm () {
+      console.log('Checkbox: ' + this.interest)
+      console.log('How: ' + this.how)
+      this.interest = []
+      this.how = ''
+    },
+    validateUserName () {
+      if (this.userName === '') {
+        this.userNameValid = 'invalid'
+      } else {
+        this.userNameValid = 'valid'
+      }
+      console.log(this.userNameValid)
+    }
+  }
+}
+</script>
 
-<style scoped>
+<style scoped lang="scss">
 form {
   margin: 2rem auto;
   max-width: 40rem;
@@ -63,6 +94,14 @@ form {
 }
 .form-control {
   margin: 0.5rem 0;
+}
+.form-control.invalid {
+  input {
+    border-color: red;
+  }
+  label {
+    color: red;
+  }
 }
 label {
   font-weight: bold;
