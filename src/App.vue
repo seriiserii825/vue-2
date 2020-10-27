4a1c57d4b8932fd9899283158ccb5a01bdccb370
serii <seriiburduja@gmail.com>
2020-10-27 12:48:14 +0200

    add files for project

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,17 @@
 <template>
   <div id="app">
-    <base-card title="How was you learning experience?">
-      <the-form @get-user-data="getUserData"></the-form>
-    </base-card>
-    <base-card title="Submited Experience">
-      <answer
-        :experiences="experiences"
-      ></answer>
-    </base-card>
+    <base-container title="Vuex"></base-container>
   </div>
 </template>
 
 <script>
-import TheForm from '@/components/Layouts/TheForm'
-import Answer from '@/components/Answer'
+import BaseContainer from './components/BaseContainer.vue';
 
 export default {
-  data () {
-    return {
-      experiences: []
-    }
-  },
-  methods: {
-    getUserData (userData) {
-      const generatedId = Date.now()
-      const experienceItem = {
-        id: generatedId,
-        name: userData.name,
-        experienceLevel: userData.experienceLevel,
-        defaultString: ' rate the learning experience '
-      }
-
-      this.experiences.unshift(experienceItem)
-    }
-  },
   components: {
-    TheForm,
-    Answer
-  }
-}
+    BaseContainer,
+  },
+};
 </script>
 
 <style>
@@ -50,8 +23,5 @@ html {
 }
 body {
   margin: 0;
-}
-ul {
-  list-style-type: none;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import BaseHeader from '@/components/Layouts/BaseHeader'
-import BaseCard from '@/components/UI/BaseCard'
-import BaseButton from '@/components/UI/BaseButton'
 
 Vue.config.productionTip = false
-
-Vue.component('base-header', BaseHeader)
-Vue.component('base-card', BaseCard)
-Vue.component('base-button', BaseButton)
 
 new Vue({
   render: h => h(App)
